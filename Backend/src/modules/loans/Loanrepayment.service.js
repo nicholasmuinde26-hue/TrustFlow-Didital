405f11acdbd31318acf0ca294e5a45fc99df099a
nicholasmuinde26-hue <nicholasmuinde26@gmail.com>
2026-08-20 23:22:15 +0300

    fix: correct loanPayment model export and import case

--- Backend/src/modules/loans/Loanrepayment.service.js
+++ Backend/src/modules/loans/Loanrepayment.service.js
@@ -1,5 +1,5 @@
 import ChamaLoan from '../../models/ChamaLoan.js';
-import LoanPayment from '../../models/Loanpayment.js';
+import LoanPayment from '../../models/loanPayment.js';
 import AppError from '../../utils/AppError.js';
 import { getOrCreatePolicy } from './loanpolicy.service.js';
 import { recalculateSchedule } from './Loanpenalty.service.js';
