d6eaa057043042d1f441994c0f3378aeb6379b71
nicholasmuinde26-hue <nicholasmuinde26@gmail.com>
2026-09-05 14:02:58 +0300

    fix: correct case for AppProvider and burial pages - linux build

--- frontend/chama-manager-frontend/src/main.jsx
+++ frontend/chama-manager-frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 import App from "./app/App";
-import AppProvider from "./app/providers/AppProvider";
+import AppProvider from "./app/providers/Appprovider";
 
 
 ReactDOM
